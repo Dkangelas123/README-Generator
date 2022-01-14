b4935c6afe14fbbcd35136d878dada6a5de6c4c4
Dkangelas123 <93230917+Dkangelas123@users.noreply.github.com>
2022-01-13 21:28:43 -0600

    Add files via upload

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+# Readme File Generator
+
+# Description
+Create readme files
+
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Credits](#credits)
+
+# Installation
+N/A
+
+# Usage
+ANSWER ALL THE QUESTIONS 
+
+# Credits
+N/A
+
+# License
+N/A
+
+# Badges
+N/A
+
+# Features
+Create readme files faster
+
+# Contributing
+N/A
+
+# Test
+N/A
---
+++ Develop/index.js
@@ -0,0 +1,83 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of messages for user input
+const questions = [{
+    type: "input",
+    message: "Enter the Title of your Project: ",
+    name: "title"
+}, {
+    type: "input",
+    message: "Enter the Description of your Project: ",
+    name: "description"
+},{
+    type: "input",
+    message: "Enter your Table of Contents (Optional): ",
+    name: "table of contents"
+},{
+    type: "input",
+    message: "Enter the installation needs for this App: ",
+    name: "installation"
+},{
+    type: "input",
+    message: "Enter how to use this App: ",
+    name: "usage"
+},{
+    type: "input",
+    message: "Enter collaborations, tutorials or third party assets. Provide Links: ",
+    name: "credits"
+},{
+    type: "input",
+    message: "Enter what other developers know what they can and cannot do with your project: ",
+    name: "license"
+},{
+    type: "input",
+    message: "Enter badges to let know other deveolpers know that you know what are you doing: ",
+    name: "badges"
+},{
+    type: "input",
+    message: "Enter Features for this app: ",
+    name: "features"
+},{
+    type: "input",
+    message: "Enter how can other developers con cutribute on your app. add guidelines to do if so: ",
+    name: "contributing"
+},{
+    type: "input",
+    message: "Write test for your app. Then provide examples on how to run them: ",
+    name: "tests"
+},
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+            
+        } else {
+            console.log(" Please wait Generating README.md ")
+        }
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+    inquirer.prompt(questions).then(function(data) {
+        writeToFile("README.md", generateMarkdown(data));
+        console.log(data)
+    })
+}
+
+// Function call to initialize app
+init();
+ 
